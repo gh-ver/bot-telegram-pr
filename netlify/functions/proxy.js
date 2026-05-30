747d43aa556b80659e29e7352b01fea5eda0d9d5
gh-ver <viktorovartem738@gmail.com>
2026-05-30 20:10:46 +0700

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,44 +1,36 @@
 export const handler = async (event, context) => {
   const url = event.queryStringParameters?.url;
-
   if (!url) {
+    
     return {
       statusCode: 400,
+
+
+      
       headers: {
         'Access-Control-Allow-Origin': '*',
+
+
+
+
+
+
+
+        
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ error: 'Missing "url" query parameter' }),
     };
-  }
-
-  try {
-    const response = await fetch(decodeURIComponent(url));
-    if (!response.ok) throw new Error(`Failed to fetch data: ${response.status}`);
-
-    const data = await response.text();
     
-    // Безопасно извлекаем тип контента, задавая дефолтное значение
-    const contentType = response.headers ? response.headers.get('content-type') : 'text/plain';
-
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, OPTIONS',
-        'Content-Type': contentType || 'text/plain',
-      },
-      body: data,
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ error: error.message }),
-    };
   }
-};
+  try {
 
+
+    
+    const response = await fetch(decodeURIComponent(url));
+    
+    if (!response.ok) throw new Error(`Failed to fetch data: ${response.status}`);
+    const datas = await response.text();
+
+    const contentType = response.headers ? response.headers.get('content-type') : 'text/plain';
+    return { statusCode: 200, headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET, OPTIONS', 'Content-Type': contentType || 'text/plain',},body: datas,};} catch (error) { return { statusCode: 500, headers: {'Access-Control-Allow-Origin': '*','Content-Type': 'application/json' },body: JSON.stringify({ error: error.message }),};}};
